e4544af85e6765be99352d2953a496f7c214bfe2
RainyDayFish <ryan.qiyu.jiang@gmail.com>
2015-05-22 10:44:29 -0400

    update comments for Entities

--- Basic Classes/Entities.java
+++ Basic Classes/Entities.java
@@ -57,7 +57,7 @@ public abstract class Entities
   /* Description of setX ()
    * This method sets a new x position of an Entity on a screen by passing a new int into the method.
    *
-   * @parameter newX An int representing the new x position of an Entity on the screen.
+   * @param newX An int representing the new x position of an Entity on the screen.
    */
   
   public void setX (int newX)
@@ -68,7 +68,7 @@ public abstract class Entities
   /* Description of setY ()
    * This method sets a new y position of an Entity on a screen by passing a new int into the method.
    *
-   * @parameter newY An int representing the new y position of an Entity on the screen.
+   * @param newY An int representing the new y position of an Entity on the screen.
    */
   
   public void setY (int newY)
@@ -79,7 +79,7 @@ public abstract class Entities
   /* Description of setImage ()
    * This method sets a new image to represent an Entity on a screen by passing a new BufferedImage into the method.
    *
-   * @parameter newImage A BufferedImage representing the Entity visually onto the screen.
+   * @param newImage A BufferedImage representing the Entity visually onto the screen.
    */
   
   public void setImage (BufferedImage newImage)
@@ -90,8 +90,8 @@ public abstract class Entities
   /*
    * This constructor is not used to instantiate an Entities object, rather to assign values to the x and y values of an object which extends Entities.
    *
-   * @parameter x An int value which is assigned to the instance variable x.
-   * @parameter y An int value which is assigned to the instance variable y.
+   * @param x An int value which is assigned to the instance variable x.
+   * @param y An int value which is assigned to the instance variable y.
    */
   
   public Entities (int x, int y)
@@ -103,9 +103,9 @@ public abstract class Entities
   /*
    * This constructor is not used to instantiate an Entities object, rather to assign values to the x, y and image values of an object which extends Entities.
    *
-   * @parameter x An int value which is assigned to the instance variable x, setting an x location of the object.
-   * @parameter y An int value which is assigned to the instance variable y, settting a y location of the object.
-   * @parameter image A BufferedImage which is assigned to the instance variable image, setting a visual representation for the object.
+   * @param x An int value which is assigned to the instance variable x, setting an x location of the object.
+   * @param y An int value which is assigned to the instance variable y, settting a y location of the object.
+   * @param image A BufferedImage which is assigned to the instance variable image, setting a visual representation for the object.
    */
   
   public Entities (int x, int y, BufferedImage image)
