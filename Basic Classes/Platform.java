7d7173fdbe18514fdc597c2b5ddeadead44defab
atarw <Atharva@Atharvas-Mac-mini.local>
2015-05-22 07:59:26 -0400

    Added Platform Comments

--- Basic Classes/Platform.java
+++ Basic Classes/Platform.java
@@ -1,20 +1,50 @@
+/* This class is used to instantiate various platforms within the game for the player to jump on. Some platforms may have different images as well as different text corresponding
+ * to the image, so that the user can correctly jump to a certain platform.
+ * 
+ * @author Atharva Washimkar
+ * @version 1 05.21.15
+ * 
+ * <p>
+ * <b>Instance Variables:</b>
+ * <p>
+ * <b>text</b> A String which stores the text contained by each platform used to ensure checking that the user jumped onto a certain platform.
+ */
+
 import java.awt.*;
 
 public class Platform extends Entities
 {
   private String text;
   
+    /* Description of getText ()
+     * This method returns the String used to represent the text contained by each platform.
+     *
+     * @return A String that represents the text contained by each platform.
+     */
+    
   public String getText ()
   {
     return text;
   }
   
-  
+    /* Description of setText ()
+     * This method sets the text of the platform based on the String parameter passed.
+     *
+     * @parameter newText A String that represents the text to be contained by a platform.
+     */
+    
   public void setText (String newText)
   {
     text = newText;
   }
   
+    /*
+     * This contructor creates a new PLatform object with it's coordinates on the screen, as well as the text, through passed parameters.
+     *
+     * @parameter x An int representing the platform's x location on the screen.
+     * @parameter y An int representing the platform's y location on the screen.
+     * @parameter text A String representing the text contained by a platform, used for checking if a user has landed on a certain platform.
+     */
   
   public Platform (int x, int y, String text)
   {
@@ -22,7 +52,15 @@ public class Platform extends Entities
     this.text = text;
   }
   
-  
+    /*
+     * This contructor creates a new PLatform object with it's coordinates on the screen, an image to represent the Platform, as well as the text, through passed parameters.
+     *
+     * @parameter x An int representing the platform's x location on the screen.
+     * @parameter y An int representing the platform's y location on the screen.
+     * @parameter text A String representing the text contained by a platform, used for checking if a user has landed on a certain platform.
+     * @parameter image A BufferedImage which is used to represent the Platform object visually on the screen.
+     */
+    
   public Platform (int x, int y, String text, BufferedImage image)
   {
     super (x, y, image);
